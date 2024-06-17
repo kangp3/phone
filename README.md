96bff36651235a9d8c88980fd6fbf0f53b0f8dd7
Peter Kang <peterjkang@gmail.com>
2024-06-17 17:37:23 -0400

    Get off-hook detection working

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ SSH
 ssh recurse@peterpi.local
 ```
 
+Dial tone generator
+===================
+```
+speaker-test -t sine -f 350 -l0
+speaker-test -t sine -f 440 -l0
+```
+
 Relevant Material
 =================
 - [SLIC datasheet](https://silvertel.com/images/datasheets/Ag1171-datasheet-Low-cost-ringing-SLIC-with-single-supply.pdf)
--- pwm.py
+++ pwm.py
@@ -1,17 +1,49 @@
+from threading import Event
 from time import sleep
 from signal import pause
 
 from gpiozero import PWMOutputDevice
+from gpiozero import DigitalInputDevice
 from gpiozero import DigitalOutputDevice
+
+
+shk_pin = DigitalInputDevice(pin=27, bounce_time=0.01)
 
 rm_dev = DigitalOutputDevice(pin=17)   # RM (ringing mode) pin
 fr_dev = PWMOutputDevice(pin=12, active_high=False, frequency=20, initial_value=0)  # FR (forward/reverse) pin
 
-while True:
-    rm_dev.on()
-    fr_dev.value = 0.5
-    sleep(1)
 
-    rm_dev.off()
-    fr_dev.value = 0
-    sleep(3)
+OFF_HOOK = Event()
+
+
+def ring_me():
+    while not OFF_HOOK.is_set():
+        rm_dev.on()
+        fr_dev.value = 0.5
+        OFF_HOOK.wait(10)
+
+        if OFF_HOOK.is_set():
+            rm_dev.off()
+            fr_dev.value = 0
+            break
+
+        rm_dev.off()
+        fr_dev.value = 0
+        OFF_HOOK.wait(3)
+
+
+def main():
+    print("Running")
+
+    def stop_ringing():
+        print("Stopping ringing?")
+        OFF_HOOK.set()
+        print("Canceled ringing?")
+
+    shk_pin.when_activated = stop_ringing
+
+    ring_me()
+
+
+if __name__ == "__main__":
+    main()
