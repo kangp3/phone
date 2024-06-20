47e128b24dd21e13c94f9fb96e80c6f4095d0360
Peter Kang <peterjkang@gmail.com>
2024-06-20 12:40:12 -0400

    Simple ringing script

---
+++ ring.py
@@ -0,0 +1,24 @@
+from time import sleep
+import sys
+
+from gpiozero import PWMOutputDevice
+from gpiozero import DigitalOutputDevice
+
+
+rm_dev = DigitalOutputDevice(pin=17)   # RM (ringing mode) pin
+fr_dev = PWMOutputDevice(  # FR (forward/reverse) pin
+    pin=12,
+    active_high=False,
+    frequency=20,
+    initial_value=0,
+)
+
+
+while True:
+    rm_dev.on()
+    fr_dev.value = 0.5
+    sleep(1)
+
+    rm_dev.off()
+    fr_dev.value = 0
+    sleep(1)
