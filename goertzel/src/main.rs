1166b6ff42a4094aaab68560376072934cd36343
Peter Kang <peterjkang@gmail.com>
2024-07-24 00:10:06 -0400

    Detect digits live

--- goertzel/src/main.rs
+++ goertzel/src/main.rs
@@ -1,7 +1,8 @@
 use std::f64::consts;
+use std::sync::mpsc::channel;
 
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use hound;
-use itertools::Itertools;
 
 
 const FNAME: &str = "/Users/kangp3/Documents/projects/phone/audio_samples/cortelco_48k.wav";
@@ -36,40 +37,73 @@ fn main() {
     let mag_threshold = 42.5_f64.exp();
     let coeffs = FREQS.map(|f| goertzel_coeff(f, SAMPLE_FREQ));
 
+    let host = cpal::default_host();
+    let device = host.default_output_device().unwrap();
+    let supported_config = device
+        .supported_output_configs()
+        .unwrap()
+        .filter_map(|r| r.try_with_sample_rate(cpal::SampleRate(SAMPLE_FREQ)))
+        .next()
+        .unwrap();
+
     let mut reader = hound::WavReader::open(FNAME).unwrap();
-    dbg!(reader.spec());
     let samples = reader.samples::<i32>();
 
-    let mut found_digits = vec![];
-    for (idx, chunk) in samples.step_by(2).chunks(WINDOW_INTERVAL as usize).into_iter().enumerate() {
-        let idx = idx * WINDOW_INTERVAL as usize;
-        let chunk = chunk.collect::<Result<Vec<i32>, _>>().unwrap();
+    let mut playback_reader = hound::WavReader::open(FNAME).unwrap();
+    let playback_samples = playback_reader.samples::<i32>().collect::<Result<Vec<_>, _>>().unwrap();
+    let mut idx = 0;
 
-        let active_freqs: Vec<_> = coeffs
-            .iter()
-            .map(|c| goertzel_me(&chunk, 0.0, 0.0, *c).0)
-            .enumerate()
-            .filter_map(|(idx, mag)| (mag > mag_threshold).then_some(idx))
-            .collect();
-        let digit = match active_freqs[..] {
-            [0, 4] => 1,
-            [0, 5] => 2,
-            [0, 6] => 3,
-            [1, 4] => 4,
-            [1, 5] => 5,
-            [1, 6] => 6,
-            [2, 4] => 7,
-            [2, 5] => 8,
-            [2, 6] => 9,
-            [3, 4] => 10,
-            [3, 5] => 11,
-            [3, 6] => 12,
-            _ => 0,
-        };
-        if digit != 0 {
-            dbg!(digit);
-            found_digits.push(digit);
+    let (send_ch, rcv_ch) = channel::<i32>();
+    let playback_stream = device.build_output_stream(
+        &supported_config.into(),
+        move |data: &mut[f32], _: &cpal::OutputCallbackInfo| {
+            for sample in data.iter_mut() {
+                let next_sample = playback_samples[idx];
+
+                // Only send the left channel into the send channel
+                if idx % 2 == 0 {
+                    send_ch.send(next_sample).unwrap();
+                }
+                *sample = next_sample as f32;
+
+                idx += 1;
+            }
+        },
+        move |_| { dbg!("Fuck error handling"); },
+        None
+    ).unwrap();
+
+    playback_stream.play().unwrap();
+
+    let mut chunk = vec![];
+    while let Ok(sample) = rcv_ch.recv() {
+        chunk.push(sample);
+        if chunk.len() == WINDOW_INTERVAL as usize {
+            let active_freqs: Vec<_> = coeffs
+                .iter()
+                .map(|c| goertzel_me(&chunk, 0.0, 0.0, *c).0)
+                .enumerate()
+                .filter_map(|(idx, mag)| (mag > mag_threshold).then_some(idx))
+                .collect();
+            let digit = match active_freqs[..] {
+                [0, 4] => 1,
+                [0, 5] => 2,
+                [0, 6] => 3,
+                [1, 4] => 4,
+                [1, 5] => 5,
+                [1, 6] => 6,
+                [2, 4] => 7,
+                [2, 5] => 8,
+                [2, 6] => 9,
+                [3, 4] => 10,
+                [3, 5] => 11,
+                [3, 6] => 12,
+                _ => 0,
+            };
+            if digit != 0 {
+                dbg!(digit);
+            }
+            chunk = vec![];
         }
-    };
-    dbg!(found_digits);
+    }
 }
