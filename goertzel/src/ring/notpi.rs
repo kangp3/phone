32f03da4cab06fc43532fa6075d215155033dbfc
Peter Kang <peterjkang@gmail.com>
2024-09-25 02:43:35 -0400

    Ring da bell

--- goertzel/src/ring/notpi.rs
+++ goertzel/src/ring/notpi.rs
@@ -11,7 +11,7 @@ use crate::asyncutil::and_log_err;
 pub fn ring_phone() -> Result<AbortHandle, Box<dyn Error>> {
     Ok(tokio::spawn(and_log_err("ringing", async move {
         loop {
-            info!("Ring ring");
+            info!("Ring ring\x07");
             sleep(Duration::from_secs(1)).await;
 
             info!("No ring ring");
