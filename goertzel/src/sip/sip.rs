d6969ab03ef8d57f9149c307e145c8bc7cc4b900
Peter Kang <peterjkang@gmail.com>
2024-09-26 03:11:00 -0400

    Ok it's working now to port it to the new

--- goertzel/src/sip/sip.rs
+++ goertzel/src/sip/sip.rs
@@ -1,111 +1,45 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::sync::LazyLock;
+use std::sync::{Arc, LazyLock};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 use local_ip_address::local_ip;
 use md5::{Md5, Digest};
-use rsip::headers::auth::{self, Algorithm, AuthQop};
-use rsip::headers::{CallId, ContentLength, Expires, MaxForwards, UserAgent};
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use rsip::headers::auth::{Algorithm, AuthQop};
+use rsip::headers::{auth, CallId, ContentLength, Expires, MaxForwards, UserAgent};
 use rsip::param::OtherParam;
 use rsip::prelude::ToTypedHeader;
 use rsip::typed::{Allow, Authorization, CSeq, Contact, From, To, Via};
 use rsip::{Auth, Header, Headers, Method, Param, Request, Scheme, SipMessage, Transport, Uri, Version};
-use tokio::sync::{broadcast, mpsc};
+use tokio::sync::{mpsc, RwLock};
 use tracing::debug;
 
 
+const MESSAGE_CHANNEL_SIZE: usize = 64;
+
+const USERNAME: &str = "1100";
+const PASSWORD: &str = "SW2fur7facrarac";
 const USER_AGENT: &str = "Frandline";
 const UA_VERSION: &str = "0.1.0";
-
-const BRANCH: &str = "branch-jiogadrbocaw";
-const TAG: &str = "tag-rjiowuqyropdrbocjwort";
-const CNONCE: &str = "cnonce-ruitodabicawerioajwefiojodsfd";
-const CALL_ID: &str = "call-id-gjiorbjcohquwrtioquweoruieo";
+// Branch should always be prefixed with magic string z9hG4bK
+// https://www.ietf.org/rfc/rfc3261.txt (8.1.1.7)
+const BRANCH_PREFIX: &str = "z9hG4bK";
+const MAX_FORWARDS: u32 = 70;
 
 // TODO(peter): Make these configurable
 pub static SERVER_ADDR: LazyLock<SocketAddr> = LazyLock::new(|| {
     SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192, 168, 12, 182)), 5060)
 });
-const USERNAME: &str = "1100";
-const PASSWORD: &str = "SW2fur7facrarac";
+pub static CLIENT_ADDR: LazyLock<SocketAddr> = LazyLock::new(|| {
+    SocketAddr::new(local_ip().unwrap(), 5060)
+});
 
-pub fn register_request() -> SipMessage {
-    let server_ip = (*SERVER_ADDR).ip();
-    let my_ip = local_ip().unwrap();
-    let socket_addr = SocketAddr::new(my_ip, 5060);
-
-    let mut headers: Headers = Default::default();
-    headers.push(CSeq{ seq: 1, method: Method::Register }.into());
-    headers.push(Via{
-        version: Version::V2,
-        transport: Transport::Udp,
-        uri: Uri {
-            host_with_port: socket_addr.into(),
-            ..Default::default()
-        },
-        params: vec![
-            Param::Branch(BRANCH.into()),
-            Param::Other(OtherParam::from("rport"), None)
-        ],
-    }.into());
-    headers.push(UserAgent::from(format!("{}/{}", USER_AGENT, UA_VERSION)).into());
-    headers.push(From{
-        display_name: Some(USERNAME.into()),
-        uri: Uri {
-            scheme: Some(Scheme::Sip),
-            host_with_port: server_ip.into(),
-            auth: Some(Auth{
-                user: USERNAME.into(),
-                password: None,
-            }),
-            ..Default::default()
-        },
-        params: vec![Param::Tag(TAG.into())],
-    }.into());
-    headers.push(CallId::from(CALL_ID).into());
-    headers.push(To{
-        display_name: None,
-        uri: Uri {
-            scheme: Some(Scheme::Sip),
-            host_with_port: server_ip.into(),
-            auth: Some(Auth{
-                user: USERNAME.into(),
-                password: None,
-            }),
-            ..Default::default()
-        },
-        params: vec![],
-    }.into());
-    headers.push(Contact{
-        display_name: Some(USERNAME.into()),
-        uri: Uri {
-            scheme: Some(Scheme::Sip),
-            host_with_port: server_ip.into(),
-            auth: Some(Auth{
-                user: USERNAME.into(),
-                password: None,
-            }),
-            ..Default::default()
-        },
-        params: vec![Param::Q("1".into())],
-    }.into());
-    headers.push(Allow::from(Method::all()).into());
-    headers.push(Expires::from(3600).into());
-    headers.push(ContentLength::from(0).into());
-    headers.push(MaxForwards::from(70).into());
-
-    SipMessage::Request(Request {
-        method: Method::Register,
-        uri: Uri {
-            scheme: Some(Scheme::Sip),
-            host_with_port: server_ip.into(),
-            ..Default::default()
-        },
-        headers,
-        version: Version::V2,
-        body: vec![],
-    })
-}
+pub static TXN_MAILBOXES: LazyLock<Arc<RwLock<HashMap::<String, mpsc::Sender<SipMessage>>>>> = LazyLock::new(|| {
+    Arc::new(RwLock::new(HashMap::new()))
+});
 
 fn md5(s: String) -> String {
     let mut hasher = Md5::new();
@@ -113,112 +47,15 @@ fn md5(s: String) -> String {
     format!("{:x}", hasher.finalize())
 }
 
-pub fn authed_register_request(opaque: Option<String>, nonce: String) -> SipMessage {
-    let server_ip = (*SERVER_ADDR).ip();
-    let my_ip = local_ip().unwrap();
-    let socket_addr = SocketAddr::new(my_ip, 5060);
-
-    let cnonce = String::from(CNONCE);
-    let nc = 1;
-
-    let ha1 = md5(format!("{}:{}:{}", USERNAME, "asterisk", PASSWORD));
-    let ha2 = md5(format!("REGISTER:sip:{}", (*SERVER_ADDR).ip()));
-    let response = md5(format!("{}:{}:{:08x}:{}:auth:{}", ha1, nonce, nc, cnonce, ha2));
-
-    let mut headers: Headers = Default::default();
-    headers.push(CSeq{ seq: 2, method: Method::Register }.into());
-    headers.push(Via{
-        version: Version::V2,
-        transport: Transport::Udp,
-        uri: Uri {
-            host_with_port: socket_addr.into(),
-            ..Default::default()
-        },
-        params: vec![
-            Param::Branch(BRANCH.into()),
-            Param::Other(OtherParam::from("rport"), None)
-        ],
-    }.into());
-    headers.push(UserAgent::from("Frandline/0.1.0").into());
-    headers.push(Authorization{
-        scheme: auth::Scheme::Digest,
-        username: USERNAME.into(),
-        realm: "asterisk".to_string(),
-        nonce,
-        uri: Uri {
-            scheme: Some(Scheme::Sip),
-            host_with_port: server_ip.into(),
-            auth: None,
-            ..Default::default()
-        },
-        response,
-        algorithm: Some(Algorithm::Md5),
-        opaque,
-        qop: Some(AuthQop::Auth { cnonce, nc }),
-    }.into());
-    headers.push(From{
-        display_name: None,
-        uri: Uri {
-            scheme: Some(Scheme::Sip),
-            host_with_port: server_ip.into(),
-            auth: Some(Auth{
-                user: USERNAME.into(),
-                password: None,
-            }),
-            ..Default::default()
-        },
-        params: vec![Param::Tag(TAG.into())],
-    }.into());
-    headers.push(CallId::from(CALL_ID).into());
-    headers.push(To{
-        display_name: None,
-        uri: Uri {
-            scheme: Some(Scheme::Sip),
-            host_with_port: server_ip.into(),
-            auth: Some(Auth{
-                user: USERNAME.into(),
-                password: None,
-            }),
-            ..Default::default()
-        },
-        params: vec![],
-    }.into());
-    headers.push(Contact{
-        display_name: None,
-        uri: Uri {
-            scheme: Some(Scheme::Sip),
-            host_with_port: my_ip.into(),
-            auth: Some(Auth{
-                user: USERNAME.into(),
-                password: None,
-            }),
-            ..Default::default()
-        },
-        params: vec![Param::Q("1".into())],
-    }.into());
-    headers.push(Allow::from(Method::all()).into());
-    headers.push(Expires::from(3600).into());
-    headers.push(ContentLength::from(0).into());
-    headers.push(MaxForwards::from(70).into());
-
-    SipMessage::Request(Request {
-        method: Method::Register,
-        uri: Uri {
-            scheme: Some(Scheme::Sip),
-            host_with_port: server_ip.into(),
-            ..Default::default()
-        },
-        headers,
-        version: Version::V2,
-        body: vec![],
-    })
-}
-
-pub async fn register(out_ch: mpsc::Sender<(SocketAddr, SipMessage)>, mut in_ch: broadcast::Receiver<(SocketAddr, SipMessage)>) -> Result<(), Box<dyn Error>> {
-    let msg = register_request();
+pub async fn register(out_ch: mpsc::Sender<SipMessage>) -> Result<(), Box<dyn Error>> {
+    // TODO(peter): Figure out how to get the rx channel into a mailbox while avoiding deadlock on
+    // the TXN_MAILBOXES
+    debug!("hello??");
+    let mut txn = Txn::new(out_ch).await;
+    let msg = SipMessage::Request(txn.register_request()?);
     debug!("{}", msg);
-    out_ch.send((*SERVER_ADDR, msg)).await?;
-    let (_, msg) = in_ch.recv().await?;
+    txn.tx_ch.send(msg).await?;
+    let msg = txn.rx_ch.recv().await.ok_or("closed rx channel")?;
     debug!("{}", msg);
     if let SipMessage::Response(r) = msg {
         let mut opaque = None;
@@ -234,9 +71,285 @@ pub async fn register(out_ch: mpsc::Sender<(SocketAddr, SipMessage)>, mut in_ch:
                 _ => {},
             }
         }
-        let req = authed_register_request(opaque, nonce);
-        debug!("AUTH REQ: {}", req);
-        out_ch.send((*SERVER_ADDR, req)).await?;
+        let msg = SipMessage::Request(txn.authed_register_request(opaque, nonce)?);
+        debug!("AUTH REQ: {}", msg);
+        txn.tx_ch.send(msg).await?;
+        let msg = txn.rx_ch.recv().await.ok_or("closed rx channel")?;
+        debug!("{}", msg);
     }
     Ok(())
+}
+
+// TODO(peter): Implement Drop on this to make sure mailboxes get cleared out
+pub struct Txn {
+    pub tx_ch: mpsc::Sender<SipMessage>,
+    pub rx_ch: mpsc::Receiver<SipMessage>,
+
+    cseq: u32,
+    call_id: CallId,
+    from_tag: Param,
+    to_tag: Option<Param>,
+}
+
+// TODO(peter): Ask about &mut impl Rng vs &mut ThreadRng (this didn't work)
+fn rand_chars(rng: &mut impl Rng, len: usize) -> String {
+    rng.sample_iter(&Alphanumeric).take(len).map(char::from).collect()
+}
+
+fn ms_since_epoch() -> u128 {
+    SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()
+}
+
+impl Txn {
+    pub async fn new(tx_ch: mpsc::Sender<SipMessage>) -> Self {
+        let (rx_send_ch, rx_ch) = mpsc::channel(MESSAGE_CHANNEL_SIZE);
+        let mut rng = thread_rng();
+        let call_id = CallId::from(format!("{}/{}", ms_since_epoch(), rand_chars(&mut rng, 16)));
+        let from_tag = Param::Tag(rand_chars(&mut rng, 16).into());
+        let to_tag = None;
+
+        {
+            TXN_MAILBOXES.clone().write().await.insert(call_id.to_string(), rx_send_ch);
+        }
+
+        Txn {
+            tx_ch,
+            rx_ch,
+
+            cseq: 0,
+            call_id,
+            from_tag,
+            to_tag,
+        }
+    }
+
+    pub fn from_req(req: Request, tx_ch: mpsc::Sender<SipMessage>, rx_ch: mpsc::Receiver<SipMessage>) -> Result<Self, Box<dyn Error>> {
+        let mut cseq: Option<u32> = None;
+        let mut call_id: Option<CallId> = None;
+        let mut from_tag: Option<Param> = None;
+        let mut to_tag: Option<Param> = None;
+
+        for header in req.headers {
+            match header {
+                Header::CallId(h) => {
+                    call_id = Some(h.into());
+                }
+                Header::CSeq(h) => {
+                    let h = h.typed()?;
+                    cseq = Some(h.seq);
+                }
+                Header::From(h) => {
+                    let h = h.typed()?;
+                    for param in h.params {
+                        match param {
+                            Param::Tag(t) => {
+                                from_tag = Some(t.into());
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                Header::To(h) => {
+                    let h = h.typed()?;
+                    for param in h.params {
+                        match param {
+                            Param::Tag(t) => {
+                                to_tag = Some(t.into());
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+                _ => {},
+            }
+        }
+
+        Ok(Txn {
+            rx_ch,
+            tx_ch,
+
+            cseq: cseq.ok_or("missing cseq")?,
+            call_id: call_id.ok_or("missing call_id")?,
+            from_tag: from_tag.ok_or("missing from tag")?,
+            to_tag,
+        })
+    }
+
+    fn new_request(&mut self, method: Method, body: Vec<u8>) -> Result<Request, Box<dyn Error>> {
+        self.cseq += 1;
+        let branch: String = format!("{}{}", BRANCH_PREFIX, rand_chars(&mut thread_rng(), 10));
+
+        let mut headers: Headers = Default::default();
+        headers.push(CSeq{ seq: self.cseq, method }.into());
+        headers.push(Via{
+            version: Version::V2,
+            transport: Transport::Udp,
+            uri: Uri {
+                host_with_port: (*CLIENT_ADDR).into(),
+                ..Default::default()
+            },
+            params: vec![
+                Param::Branch(branch.into()),
+                Param::Other(OtherParam::from("rport"), None)
+            ],
+        }.into());
+        headers.push(UserAgent::from(format!("{}/{}", USER_AGENT, UA_VERSION)).into());
+        headers.push(From{
+            display_name: Some(USERNAME.into()),
+            uri: Uri {
+                scheme: Some(Scheme::Sip),
+                host_with_port: (*SERVER_ADDR).into(),
+                auth: Some(Auth{
+                    user: USERNAME.into(),
+                    password: None,
+                }),
+                ..Default::default()
+            },
+            params: vec![self.from_tag.clone()],
+        }.into());
+        headers.push(self.call_id.clone().into());
+        headers.push(To{
+            display_name: None,
+            uri: Uri {
+                scheme: Some(Scheme::Sip),
+                host_with_port: (*SERVER_ADDR).into(),
+                auth: Some(Auth{
+                    user: USERNAME.into(),
+                    password: None,
+                }),
+                ..Default::default()
+            },
+            params: vec![self.to_tag.clone()].into_iter().filter_map(|t| t).collect(),
+        }.into());
+        headers.push(Contact{
+            display_name: Some(USERNAME.into()),
+            uri: Uri {
+                scheme: Some(Scheme::Sip),
+                host_with_port: (*SERVER_ADDR).into(),
+                auth: Some(Auth{
+                    user: USERNAME.into(),
+                    password: None,
+                }),
+                ..Default::default()
+            },
+            params: vec![Param::Q("1".into())],
+        }.into());
+        headers.push(MaxForwards::from(MAX_FORWARDS).into());
+        headers.push(ContentLength::from(body.len() as u32).into());
+
+        Ok(Request {
+            method,
+            uri: Uri {
+                scheme: Some(Scheme::Sip),
+                host_with_port: (*SERVER_ADDR).into(),
+                ..Default::default()
+            },
+            version: Version::V2,
+            headers,
+            body,
+        })
+    }
+
+    fn register_request(&mut self) -> Result<Request, Box<dyn Error>> {
+        let mut req = self.new_request(Method::Register, vec![])?;
+        req.headers.push(Allow::from(Method::all()).into());
+        Ok(req)
+    }
+
+    fn authed_register_request(&mut self, opaque: Option<String>, nonce: String) -> Result<Request, Box<dyn Error>> {
+        let cnonce = format!("{}/{}", ms_since_epoch(), rand_chars(&mut thread_rng(), 16));
+        let nc = 1;
+
+        let ha1 = md5(format!("{}:{}:{}", USERNAME, "asterisk", PASSWORD));
+        let ha2 = md5(format!("REGISTER:sip:{}", (*SERVER_ADDR)));
+        let response = md5(format!("{}:{}:{:08x}:{}:auth:{}", ha1, nonce, nc, cnonce, ha2));
+
+        let mut headers: Headers = Default::default();
+        headers.push(CSeq{ seq: 2, method: Method::Register }.into());
+        headers.push(Via{
+            version: Version::V2,
+            transport: Transport::Udp,
+            uri: Uri {
+                host_with_port: (*SERVER_ADDR).into(),
+                ..Default::default()
+            },
+            params: vec![
+                Param::Branch(format!("{}{}", BRANCH_PREFIX, rand_chars(&mut thread_rng(), 10)).into()),
+                Param::Other(OtherParam::from("rport"), None)
+            ],
+        }.into());
+        headers.push(UserAgent::from("Frandline/0.1.0").into());
+        headers.push(Authorization{
+            scheme: auth::Scheme::Digest,
+            username: USERNAME.into(),
+            realm: "asterisk".to_string(),
+            nonce,
+            uri: Uri {
+                scheme: Some(Scheme::Sip),
+                host_with_port: (*SERVER_ADDR).into(),
+                auth: None,
+                ..Default::default()
+            },
+            response,
+            algorithm: Some(Algorithm::Md5),
+            opaque,
+            qop: Some(AuthQop::Auth { cnonce, nc }),
+        }.into());
+        headers.push(From{
+            display_name: None,
+            uri: Uri {
+                scheme: Some(Scheme::Sip),
+                host_with_port: (*SERVER_ADDR).into(),
+                auth: Some(Auth{
+                    user: USERNAME.into(),
+                    password: None,
+                }),
+                ..Default::default()
+            },
+            params: vec![self.from_tag.clone()],
+        }.into());
+        headers.push(CallId::from(self.call_id.clone()).into());
+        headers.push(To{
+            display_name: None,
+            uri: Uri {
+                scheme: Some(Scheme::Sip),
+                host_with_port: (*SERVER_ADDR).into(),
+                auth: Some(Auth{
+                    user: USERNAME.into(),
+                    password: None,
+                }),
+                ..Default::default()
+            },
+            params: vec![],
+        }.into());
+        headers.push(Contact{
+            display_name: None,
+            uri: Uri {
+                scheme: Some(Scheme::Sip),
+                host_with_port: (*CLIENT_ADDR).into(),
+                auth: Some(Auth{
+                    user: USERNAME.into(),
+                    password: None,
+                }),
+                ..Default::default()
+            },
+            params: vec![Param::Q("1".into())],
+        }.into());
+        headers.push(Allow::from(Method::all()).into());
+        headers.push(Expires::from(3600).into());
+        headers.push(ContentLength::from(0).into());
+        headers.push(MaxForwards::from(70).into());
+
+        Ok(Request {
+            method: Method::Register,
+            uri: Uri {
+                scheme: Some(Scheme::Sip),
+                host_with_port: (*SERVER_ADDR).into(),
+                ..Default::default()
+            },
+            headers,
+            version: Version::V2,
+            body: vec![],
+        })
+    }
 }
