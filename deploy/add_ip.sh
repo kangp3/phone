a48e869aec526523b437ebad577d8ac94f052966
Peter Kang <peterjkang@gmail.com>
2025-08-17 23:20:47 -0400

    Clean up existing rules from security group

--- deploy/add_ip.sh
+++ deploy/add_ip.sh
@@ -1,37 +1,54 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Get security group ID
+security_group_id="$(
+  aws ec2 describe-instances |
+    jq -r '.Reservations[0].Instances[0].SecurityGroups[0].GroupId'
+)"
+
+# Delete existing ingress rules
+rule_ids=$(
+  aws ec2 describe-security-group-rules \
+    --filters "Name=group-id,Values=$security_group_id" |
+    jq -r '.SecurityGroupRules[] | select(.IsEgress==false) | .SecurityGroupRuleId'
+)
+if [[ $rule_ids ]]
+then
+  aws ec2 revoke-security-group-ingress \
+    --group-id "$security_group_id" \
+    --security-group-rule-ids $rule_ids \
+    >/dev/null
+fi
+
+# Add laptop IP
 my_ip="$(curl --silent https://ipinfo.io/ip)"
 today="$(date +'%Y/%m/%d')"
 
 tcp_permissions_vals=(
-	'IpProtocol=tcp'
-	'FromPort=0'
-	'ToPort=65535'
-	"IpRanges=[{CidrIp=${my_ip}/32,Description='${today} Peter Laptop'}]"
+  'IpProtocol=tcp'
+  'FromPort=0'
+  'ToPort=65535'
+  "IpRanges=[{CidrIp=${my_ip}/32,Description='${today} Peter Laptop'}]"
 )
 tcp_permissions=$(IFS=,; echo "${tcp_permissions_vals[*]}")
 
 udp_permissions_vals=(
-	'IpProtocol=udp'
-	'FromPort=0'
-	'ToPort=65535'
-	"IpRanges=[{CidrIp=${my_ip}/32,Description='${today} Peter Laptop'}]"
+  'IpProtocol=udp'
+  'FromPort=0'
+  'ToPort=65535'
+  "IpRanges=[{CidrIp=${my_ip}/32,Description='${today} Peter Laptop'}]"
 )
 udp_permissions=$(IFS=,; echo "${udp_permissions_vals[*]}")
 
-security_group_id="$(
-	aws ec2 describe-instances |
-		jq -r '.Reservations[0].Instances[0].SecurityGroups[0].GroupId'
-)"
 aws ec2 authorize-security-group-ingress \
-	--group-id "$security_group_id" \
-	--ip-permissions "${tcp_permissions}" \
-	>/dev/null
+  --group-id "$security_group_id" \
+  --ip-permissions "${tcp_permissions}" \
+  >/dev/null
 >&2 echo "Created TCP permissions for ${my_ip}"
 aws ec2 authorize-security-group-ingress \
-	--group-id "$security_group_id" \
-	--ip-permissions "${udp_permissions}" \
-	>/dev/null
+  --group-id "$security_group_id" \
+  --ip-permissions "${udp_permissions}" \
+  >/dev/null
 >&2 echo "Created UDP permissions for ${my_ip}"
-	>/dev/null
+  >/dev/null
